adab81524ca0a7db629c4ea7437d71e4695fb6fe
Priyanka Katre <priyankack1707@gmail.com>
2024-11-26 23:21:48 +0000

    Setup MongoDB connection

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,6 +13,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+//3VX0Q8Nazju7VlKk
+
 const Login = () => {
     const [signupInput, setsignupInput] = useState({
         name: '',
