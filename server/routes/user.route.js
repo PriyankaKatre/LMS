bf853736df00bf4bfa752cca0cb70500cde370e8
Priyanka Katre <priyankack1707@gmail.com>
2024-12-03 21:46:59 +0000

    Add logout route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
   register,
-  login,
+    login,
+  logout,
   getUserProfile,
 } from "../controllers/user.controlles.js";
 import isAutheticated from "../middleware/isAutheticated.js";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").post(logout);
 router.route("/profile").get(isAutheticated, getUserProfile);
 
 export default router;
